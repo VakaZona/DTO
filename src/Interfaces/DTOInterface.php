0cd83106e3ce5adfa975ffa597ab97895a9456e1
Valery Zonov <valery.zonov@sibers.com>
2023-10-09 16:37:44 +0700

    Add methods to interface

--- src/Interfaces/DTOInterface.php
+++ src/Interfaces/DTOInterface.php
@@ -7,5 +7,15 @@ interface DTOInterface
     /**
      * @param array<string, mixed> $data
      */
-    public function __construct(array $data=[]);
+    public function __construct(array $data = []);
+
+    public function toArray():array;
+
+    public function toJson():string;
+
+    public function jsonSerialize(): array;
+
+    public function getValues(): array;
+
+    public function isSet(string $key):bool;
 }
